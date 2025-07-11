49a71f8caa60912da3f5615198d1ed9031fd0a3c
M <toyamax2020@gmail.com>
2025-07-11 09:51:18 +0900

    追加

--- src/main/java/com/example/sample/controller/HelloController.java
+++ src/main/java/com/example/sample/controller/HelloController.java
@@ -1,13 +1,15 @@
 package com.example.sample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("welcome","文字追加");
         return "hello";
     }
 }
