fdf3fa2802783dfb203bbfd96e12eb433f5375b6
M <toyamax2020@gmail.com>
2025-07-12 12:47:57 +0900

    パラメた

--- src/main/java/com/example/sample/controller/HelloController.java
+++ src/main/java/com/example/sample/controller/HelloController.java
@@ -3,13 +3,16 @@ package com.example.sample.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(Model model) {
+    public String hello(Model model, @RequestParam(required = false, defaultValue = "testのパラメータなし") String test) {
         model.addAttribute("welcome","文字追加");
+        model.addAttribute("test",test);
+
         return "hello";
     }
 }
