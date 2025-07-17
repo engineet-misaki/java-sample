e9b65aa08d6fda9ea15d079158d8e85ad7f55a2f
M <toyamax2020@gmail.com>
2025-07-17 23:24:02 +0900

    昨日のやつ

--- src/main/java/com/example/sample/controller/HelloController.java
+++ src/main/java/com/example/sample/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.sample.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,6 +13,13 @@ public class HelloController {
     public String hello(Model model, @RequestParam(required = false, defaultValue = "testのパラメータなし") String test) {
         model.addAttribute("welcome","文字追加");
         model.addAttribute("test",test);
+
+        return "hello";
+    }
+
+    @PostMapping("/post")
+    public String post(@RequestParam String greeting, Model model) {
+        model.addAttribute("welcome", greeting);
 
         return "hello";
     }
